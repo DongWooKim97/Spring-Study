9dd17bf7dcb2ccf18dbfab73eb795e0f6da09c44
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-06 23:39:00 +0900

    0406 스프링 핵심 원리 이해2 완료

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -3,16 +3,22 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // 객체 생성한걸 다 BEAN에 넣어 설정해줌
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println("new member =" + member.getName());
+        System.out.println("new member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
     }
 }
