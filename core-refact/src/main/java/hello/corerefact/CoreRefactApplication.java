fbe8292a9574c4c92224e02a6972c2baa12a9db4
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-18 00:50:44 +0900

    ì0418 수업 복기 시작

---
+++ core-refact/src/main/java/hello/corerefact/CoreRefactApplication.java
@@ -0,0 +1,13 @@
+package hello.corerefact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreRefactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreRefactApplication.class, args);
+    }
+
+}
