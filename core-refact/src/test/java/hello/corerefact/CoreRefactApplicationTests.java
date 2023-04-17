1a9f7919a2424e96780d23b27fa53daeb52f293c
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-18 00:52:31 +0900

    ì0418 수업 복기 시작

---
+++ core-refact/src/test/java/hello/corerefact/CoreRefactApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.corerefact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreRefactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
