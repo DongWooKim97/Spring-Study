0edafbbebcef8e36176bf5d43655b778c1f753ab
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-17 23:39:23 +0900

    0417 강의 내용

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private final int discountPercent = 10;
