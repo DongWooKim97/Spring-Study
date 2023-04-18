313a9e495bbb0c0c859536c42fef4632329b01c3
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-18 10:02:31 +0900

    0418 옵션처리

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        어디서 찾을지 탐색위치를 지정한다. 아래는 디렉토리 위치
+//        디폴트(지정안하면?)는 ComponentScan이 붙어있는 패키지의 위치 하위로 다 훑는다!
+//        설정 정보 클래스의 위치를 프로젝트 최상단에 두는 것이 좋다
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
