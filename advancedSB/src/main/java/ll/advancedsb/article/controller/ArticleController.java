11bc66aefc5be2ea6011ad521a8c72f8e6ae8596
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-04-26 17:19:05 +0900

    아티클 컨트롤러 추가

--- advancedSB/src/main/java/ll/advancedsb/article/controller/ArticleController.java
+++ advancedSB/src/main/java/ll/advancedsb/article/controller/ArticleController.java
@@ -1,4 +1,24 @@
 package ll.advancedsb.article.controller;
 
+import ll.advancedsb.article.entity.Article;
+import ll.advancedsb.article.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/article")
 public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @RequestMapping("/getList")
+    @ResponseBody
+    public List<Article> getList() {
+        return articleService.getList();
+    }
 }
